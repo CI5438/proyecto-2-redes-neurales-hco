fa6166bde55be756c449aa7830558fd899956213
Rafael Blanco <rblanco25@icloud.com>
2018-06-18 10:30:45 -0430

    Error acumulado

--- main.py
+++ main.py
@@ -53,6 +53,7 @@ class Network:
         self.layers = []
         self.weights = []
         self.x0_weights = []
+        self.err = []
 
         self.init_layers(q)
         self.init_weights(q)
@@ -67,30 +68,27 @@ class Network:
         for i in range(len(q)-1):
             aux = []
             for j in range(q[i]):
-                #aux.append([uniform(-1.0, 1.0) for k in range(q[i+1])])
-                aux.append([uniform(-0.5, 0.5) for k in range(q[i+1])])
-                #aux.append([0.5 for k in range(q[i+1])])
-            
+                aux.append([uniform(-0.5, 0.5) for k in range(q[i+1])])        
             self.weights.append(aux)
 
         for i in range(1, len(q)):
-            #self.x0_weights.append([uniform(-1.0, 1.0) for k in range(q[i])])
             self.x0_weights.append([uniform(-0.5, 0.5) for k in range(q[i])])
-            #self.x0_weights.append([0.5 for k in range(q[i])])
 
         return True
 
     def training(self, n, t, goal):
         #t,goal = read_dataset(nombre)
+        self.err = []
         it = 1
         aciertos=0
         desaciertos=0
-        max_it = 1000
+        max_it = 3000
         epsilon = 10**-5
         w_new=[1]
         w_old=[2]
         print("Inicio de Entrenamiento")
         while ((norm2(sub_vec(w_new,w_old))>epsilon) and (it<max_it)): #para todos los conjuntos de ejemplos
+            acum=0.0
             for i in range(len(t)):  #para cada ejemplo
                 o=self.get_o(t[i])
                 s=self.get_s(o, goal[i])
@@ -99,14 +97,19 @@ class Network:
                     aciertos=aciertos+1
                 else:
                     desaciertos=desaciertos+1
-
+                for j in range(len(o[len(o)-1])):
+                    acum = acum + ((o[len(o)-1][j]-goal[i][j])**2)
+            self.err.append(acum/2)
             it=it+1
         print("Entrenamiento terminado.\n numero de neuronas capa intermedia: "+str(len(self.layers[1]))+"\n numero de iteraciones: "+str(it)+"\n tasa de aprendizaje: "+str(n))
-        efec=aciertos*100/(aciertos+desaciertos)
-        print("Estadisticas durante entrenamiento:")
-        print("Casos acertados: "+str(aciertos)+" ,Casos no acertados: "+str(desaciertos)+" Efectividad: "+str(efec)+"%")
-        print("Estadisticas de los datos de entrenamiento: ")
-        self.eval_area(t,goal)
+        #efec=aciertos*100/(aciertos+desaciertos)
+        #print("Estadisticas durante entrenamiento:")
+        #print("Casos acertados: "+str(aciertos)+" ,Casos no acertados: "+str(desaciertos)+" Efectividad: "+str(efec)+"%")
+        #print("Estadisticas de los datos de entrenamiento: ")
+        #self.eval_area(t,goal)
+
+    def get_err(self    ):
+        return self.err
 
     def eval_area(self, t, goal):
         dentro=[]
@@ -115,6 +118,7 @@ class Network:
         desaciertos=0
         falso_positivo=0
         falso_negativo=0
+        err_acum = 0.0
         for i in range(len(t)):
             o = self.get_o(t[i])
             correcto=True
@@ -133,11 +137,13 @@ class Network:
                     falso_positivo=falso_positivo+1
                 if (round(o[len(o)-1][0],0)==0.0):
                     falso_negativo=falso_negativo+1
+            err_acum=err_acum+((o[len(o)-1][0]-goal[i][0])**2)
+        err_acum=err_acum/2
         efec=aciertos*100/(aciertos+desaciertos)
-        print("Casos acertados: "+str(aciertos)+" ,Casos no acertados: "+str(desaciertos)+" Efectividad: "+str(efec)+"%")
-        print("Falso Positivo: "+str(falso_positivo)+" Falso negativo: "+str(falso_negativo)+"\n")
+        print("Casos acertados: "+str(aciertos)+" ,Casos no acertados: "+str(desaciertos)+" ,Efectividad: "+str(efec)+"%")
+        print("Error acumulado: "+str(err_acum)+" ,Falso Positivo: "+str(falso_positivo)+" ,Falso negativo: "+str(falso_negativo)+"\n")
 
-        return dentro, fuera, aciertos, desaciertos, falso_positivo, falso_negativo
+        return dentro, fuera, aciertos, desaciertos, falso_positivo, falso_negativo, err_acum
 
     def get_o(self,example):
         o=[example]
@@ -176,13 +182,11 @@ class Network:
             for j in range(len(self.weights[i])): #para cada neurona inicial
                 for k in range(len(self.weights[i][j])): #para cada neurona final
                     w_old.append(self.weights[i][j][k])
-                    #self.weights[i][j][k] = self.weights[i][j][k] + n*s[i+1][k]*self.weights[i][j][k]*o[i][j]
                     self.weights[i][j][k] = self.weights[i][j][k] + n*s[i+1][k]*o[i][j]
                     w_new.append(self.weights[i][j][k])
         for i in range(len(self.x0_weights)): #Para cada capa
             for j in range(len(self.x0_weights[i])):#para cada peso hacia esa capa
                 w_old.append(self.x0_weights[i][j])
-                #self.x0_weights[i][j] = self.x0_weights[i][j] + n*s[i+1][j]*self.x0_weights[i][j]
                 self.x0_weights[i][j] = self.x0_weights[i][j] + n*s[i+1][j]
                 w_new.append(self.x0_weights[i][j])
         return w_old, w_new
@@ -197,20 +201,24 @@ def corrida(datos, prueba, n, tasa, neuronas_intermedia):
     desaciertos=[]
     falso_positivo=[]
     falso_negativo=[]
+    err_acum=[]
     for i in range(n):
         net = Network([2,neuronas_intermedia,1])
         net.training(tasa, x, y)
         print("Estadistica de datos de prueba: ")
-        a,b,c,d,e,f = net.eval_area(datos_prueba_x, datos_prueba_y)
+        a,b,c,d,e,f,g = net.eval_area(datos_prueba_x, datos_prueba_y)
         dentro.append(a)
         fuera.append(b)
         aciertos.append(c)
         desaciertos.append(d)
         falso_positivo.append(e)
         falso_negativo.append(f)
+        err_acum.append(g)
     print("\n\nPromedio de resultados para "+str(n)+" corridas:")
-    print("Casos acertados: "+str(promedio(aciertos))+" ,Casos no acertados: "+str(promedio(desaciertos))+" Efectividad: "+str(promedio(aciertos)*100/(promedio(aciertos)+promedio(desaciertos)))+"%")
-    print("Falso Positivo: "+str(promedio(falso_positivo))+" Falso negativo: "+str(promedio(falso_negativo))+"\n")
+    print("Casos acertados: "+str(promedio(aciertos))+" ,Casos no acertados: "+str(promedio(desaciertos))+" ,Efectividad: "+str(promedio(aciertos)*100/(promedio(aciertos)+promedio(desaciertos)))+"%")
+    print("Error acumulado: "+str(promedio(err_acum))+ " ,Falso Positivo: "+str(promedio(falso_positivo))+" ,Falso negativo: "+str(promedio(falso_negativo))+"\n")
+    #print(net.get_err())
+    #return promedio(falso_positivo), promedio(falso_negativo)
 
 # b1_archivos = ["datosP2_AJ2018_B1_N500.txt","datosP2_AJ2018_B1_N1000.txt","datosP2_AJ2018_B1_N2000.txt"]
 # b2_archivos = ["datosP2_AJ2018_B2_N500.txt","datosP2_AJ2018_B2_N1000.txt","datosP2_AJ2018_B2_N2000.txt"]
@@ -223,15 +231,9 @@ alpha = [0.1,0.2,0.3]
 data = "datosP2_AJ2018_B2_N2000.txt"
 proving = "prueba_B2_barrido_100_por_100.txt"
 
+
+h=open("results.csv", "w")
 for i in range(len(alpha)):
     print("alpha: ",alpha[i])
     print("---------------------------------------------------------------------")
-    corrida(data,proving,3,alpha[i],6)
-
-
-#n = Network([8, 3, 8])
-#x=[[1.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0],[0.0,1.0,0.0,0.0,0.0,0.0,0.0,0.0],[0.0,0.0,1.0,0.0,0.0,0.0,0.0,0.0],[0.0,0.0,0.0,1.0,0.0,0.0,0.0,0.0],[0.0,0.0,0.0,0.0,1.0,0.0,0.0,0.0],[0.0,0.0,0.0,0.0,0.0,1.0,0.0,0.0],[0.0,0.0,0.0,0.0,0.0,0.0,1.0,0.0],[0.0,0.0,0.0,0.0,0.0,0.0,0.0,1.0]]
-#y=[[1.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0],[0.0,1.0,0.0,0.0,0.0,0.0,0.0,0.0],[0.0,0.0,1.0,0.0,0.0,0.0,0.0,0.0],[0.0,0.0,0.0,1.0,0.0,0.0,0.0,0.0],[0.0,0.0,0.0,0.0,1.0,0.0,0.0,0.0],[0.0,0.0,0.0,0.0,0.0,1.0,0.0,0.0],[0.0,0.0,0.0,0.0,0.0,0.0,1.0,0.0],[0.0,0.0,0.0,0.0,0.0,0.0,0.0,1.0]]
-#n = Network([2, 2, 2])
-#x = [[0,0],[0,1],[1,0],[1,1]]
-#y = [[0,1], [1,0], [1,0], [0,1]]
+    corrida(data,proving,1,alpha[i],6)
